a1936a87e02abf3d98527df3b996a431e5b3a8d0
SACHINKUMARHACKER <sachinkum123567@gmail.com>
2019-10-28 17:22:25 +0530

    topic pub sub cpp

--- catkin_ws/src/my_package/src/topic_publisher.cpp
+++ catkin_ws/src/my_package/src/topic_publisher.cpp
@@ -1,25 +1,23 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
-
-#include <sstream>
+#include "std_msgs/Int32.h"
+#include <iostream>
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "topic_publisher");
 	ros::NodeHandle nh;
-	ros::Publisher pub = nh.advertise<std_msgs::String>("chatter", 10);
+	ros::Publisher pub = nh.advertise<std_msgs::Int32>("/counter", 10);
 	ros::Rate loop_rate(10);
-
-	int count = 0;
+	int number_count = 0;
 	while (ros::ok())
 	{
-		std::stringstream ss;
-	    ss << "hello world " << count;
-   		msg.data = ss.str();
+		std_msgs::Int32 msg;
+		msg.data = number_count;
+		ROS_INFO("%d", msg.data);
 		pub.publish(msg);
-		count = count + 1;
 		ros::spinOnce();
 		loop_rate.sleep();
+		++number_count;
 	}
 	return 0;
 }
--- catkin_ws/src/my_package/src/topic_subscriber.cpp
+++ catkin_ws/src/my_package/src/topic_subscriber.cpp
@@ -1,17 +1,17 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "std_msgs/Int32.h"
+#include <iostream>
 
-
-void FeedbackCB(const std_msgs::String::ConstPtr& msg)
+void FeedbackCB(const std_msgs::Int32::ConstPtr& msg)
 {
-	ROS_INFO("Count is: [%s]", msg->data.c_str());
+	ROS_INFO("Count is: [%d]", msg->data);
 }
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "topic_subscriber");
 	ros::NodeHandle nh;
-	ros::Subscriber sub = nh.subscribe("chatter", 10, FeedbackCB);
+	ros::Subscriber sub = nh.subscribe("/counter", 10, FeedbackCB);
 	ros::spin();
 	return 0;
 }
